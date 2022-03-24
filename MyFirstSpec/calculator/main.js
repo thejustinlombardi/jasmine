39cb92a942877b61351927c0eb69b4e47e03ef29
thejustinlombardi <thejustinlombardi@gmail.com>
2022-03-23 21:19:40 -0400

    Finish lecture 11

--- MyFirstSpec/calculator/main.js
+++ MyFirstSpec/calculator/main.js
@@ -2,4 +2,39 @@ function calculate(inputValue) {
 	// +-/*
 	// \+|\-|\*|\/
 	const expression = /\+|\-|\*|\//;
+	const numbers = inputValue.split(expression);
+
+	const numA = parseInt(numbers[0]);
+	const numB = parseInt(numbers[1]);
+
+	const operation = inputValue.match(expression);
+
+	const calculator = new Calculator();
+	calculator.add(numA);
+
+	let result;
+	switch (operation[0]) {
+		case "+":
+			result = calculator.add(numB);
+			break;
+		case "-":
+			result = calculator.subtract(numB);
+			break;
+		case "*":
+			result = calculator.multiply(numB);
+			break;
+		case "/":
+			result = calculator.divide(numB);
+			break;
+	}
+
+	updateResult(result);
+}
+
+function updateResult(result) {
+	const element = document.getElementById("result");
+
+	if (element) {
+		element.innerText = result;
+	}
 }
